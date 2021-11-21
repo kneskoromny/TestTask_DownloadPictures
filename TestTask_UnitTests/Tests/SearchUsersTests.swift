0b7fc007c809d480913174a1a9aaf1808f76d707
kneskoromny <kneskoromny@gmail.com>
2021-11-21 10:34:37 +0300

    протестировал пирсинг json по модели данных user

---
+++ TestTask_UnitTests/Tests/SearchUsersTests.swift
@@ -0,0 +1,34 @@
+//
+//  SearchUsersTests.swift
+//  TestTask_DownloadPicturesTests
+//
+//  Created by Кирилл Нескоромный on 21.11.2021.
+//
+
+import XCTest
+@testable import TestTask_DownloadPictures
+
+class SearchUsersTests: XCTestCase {
+    
+    var sut: [User]!
+
+    override func setUpWithError() throws {
+        super.setUp()
+        
+        let data = try getData(fromJSON: "User")
+        sut = try JSONDecoder().decode([User].self, from: data)
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_JSONMapping() {
+        XCTAssertNotNil(sut)
+        XCTAssertEqual(sut.first?.name, "Leanne Graham")
+        XCTAssertEqual(sut.first?.address.city, "Gwenborough")
+        XCTAssertEqual(sut.first?.company.name, "Romaguera-Crona")
+    }
+
+}
