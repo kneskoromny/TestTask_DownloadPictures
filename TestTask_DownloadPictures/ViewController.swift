33a57cd16d51d5bd3dd001bd7fd8e744e60fde42
kneskoromny <kneskoromny@gmail.com>
2021-11-21 09:06:00 +0300

    Initial Commit

---
+++ TestTask_DownloadPictures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask_DownloadPictures
+//
+//  Created by Кирилл Нескоромный on 21.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
