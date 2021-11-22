eff773c319e79a2fa0e986986cb3496b2e82405a
kneskoromny <kneskoromny@gmail.com>
2021-11-22 09:20:40 +0300

    файлы для CustomView

---
+++ TestTask_DownloadPictures/Views/CustomView/CustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView.swift
+//  TestTask_DownloadPictures
+//
+//  Created by Кирилл Нескоромный on 22.11.2021.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
